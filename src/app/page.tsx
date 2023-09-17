99ed5833033203399288fd8b80d0e45cd7a2a32a
Ryota Ikezawa <pavegy@gmail.com>
2023-09-18 02:29:05 +0900

    :recycle: Define all schemas

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,13 +11,13 @@ export default function Home() {
           <div>
             <h2 className="font-bold">Character List</h2>
             {data?.data.contents.characters.map((c: Character) => {
-              return <div key={c.localizedNames["ja-JP"]}>{c.localizedNames["ja-JP"]}</div>
+              return <div key={c.id}>{c.localizedNames && c.localizedNames["ja-JP"]}</div>
             })}
           </div>
           <div>
             <h2 className="font-bold">Map List</h2>
             {data?.data.contents.maps.map((c: Map) => {
-              return <div key={c.localizedNames["ja-JP"]}>{c.localizedNames["ja-JP"]}</div>
+              return <div key={c.id}>{c.localizedNames && c.localizedNames["ja-JP"]}</div>
             })}
           </div>
         </div>}
--- src/models/generated/api.schemas.ts
+++ src/models/generated/api.schemas.ts
@@ -23,8 +23,73 @@ export interface ContentItem {
   assetName: string;
   /** This field is only included for maps and game modes. These values are used in the match response. */
   assetPath: string;
-  localizedNames: LocalizedNames;
+  localizedNames?: LocalizedNames;
 }
+
+/**
+ * An act
+ */
+export interface Act {
+  id: string;
+  name: string;
+  isActive: boolean;
+  localizedNames?: LocalizedNames;
+}
+
+/**
+ * A player title
+ */
+export type PlayerTitle = ContentItem;
+
+/**
+ * A player card
+ */
+export type PlayerCard = ContentItem;
+
+/**
+ * A charm level
+ */
+export type CharmLevel = ContentItem;
+
+/**
+ * A charm
+ */
+export type Charm = ContentItem;
+
+/**
+ * A spray level
+ */
+export type SprayLevel = ContentItem;
+
+/**
+ * A spray
+ */
+export type Spray = ContentItem;
+
+/**
+ * A game mode
+ */
+export type GameMode = ContentItem;
+
+/**
+ * An equip
+ */
+export type Equip = ContentItem;
+
+/**
+ * A skin level
+ */
+export type SkinLevel = ContentItem;
+
+/**
+ * A skin
+ */
+export type Skin = ContentItem;
+
+/**
+ * A chroma
+ */
+export type Chroma = ContentItem;
 
 /**
  * A map
@@ -50,10 +115,35 @@ export interface BaseError {
  * A contents
  */
 export interface Content {
+  version: string;
   /** A character list */
   characters: ContentItem[];
   /** A map list */
   maps: ContentItem[];
+  /** A chroma list */
+  chromas: ContentItem[];
+  /** A skin list */
+  skins: ContentItem[];
+  /** A skin level list */
+  skinLevels: ContentItem[];
+  /** An equip list */
+  equips: ContentItem[];
+  /** A game mode list */
+  gameModes: ContentItem[];
+  /** A spray list */
+  sprays: ContentItem[];
+  /** A spray level list */
+  sprayLevels: ContentItem[];
+  /** A charm list */
+  charms: ContentItem[];
+  /** A charm level list */
+  charmLevels: ContentItem[];
+  /** A player card list */
+  playerCards: ContentItem[];
+  /** A player title list */
+  playerTitles: ContentItem[];
+  /** An act list */
+  acts: Act[];
 }
 
 /**
