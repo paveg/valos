3d6c3a66e6536b3f9fbe4bc0ecaa766ae37d3fad
Ryota Ikezawa <pavegy@gmail.com>
2023-09-23 01:18:11 +0900

    :recycle: Update

--- src/app/api/contents/route.ts
+++ src/app/api/contents/route.ts
@@ -1,5 +1,15 @@
+import axios, { AxiosInstance, AxiosResponse } from "axios";
 import { NextResponse } from "next/server";
+
+const baseURL = "https://ap.api.riotgames.com/val";
+const client: AxiosInstance = axios.create({
+  baseURL: baseURL,
+  headers: { "Content-Type": "application/json" },
+});
+
 export async function GET(request: Request, {}) {
-  // TODO: Implement actual API
-  return NextResponse.json({});
+  const res = await client.get(
+    `/content/v1/contents?api_key=${process.env.RIOT_API_KEY}`,
+  );
+  return NextResponse.json(res.data);
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,40 +1,68 @@
 "use client";
-import { Character, Map, useContents } from "@/models/generated";
+import { Character, Content, Map, useContents } from "@/models/generated";
 import React from "react";
 
 export default function Home() {
   const { data, isLoading, error } = useContents();
-  return (
-    <React.StrictMode>
-      <main>
-        {isLoading ? (
-          <>Loading...</>
-        ) : (
+  if (isLoading) return <div>Loading...</div>;
+  if (error) return <div>Error: {error.message}</div>;
+  if (data?.data) {
+    const contents = data.data as Content;
+    return (
+      <React.StrictMode>
+        <main>
           <div>
             <div>
               <h2 className="font-bold">Character List</h2>
-              {data?.data.contents.characters.map((c: Character) => {
-                return (
-                  <div key={c.id}>
-                    {c.localizedNames && c.localizedNames["ja-JP"]}
-                  </div>
-                );
-              })}
+              <table className="table-auto">
+                <thead>
+                  <th>ID</th>
+                  <th>Localized Name - ja-JP</th>
+                  <th>Localized Name - en-US</th>
+                </thead>
+                <tbody>
+                  {contents.characters
+                    .filter((c) => !c?.name.includes("Null"))
+                    .map((c: Character) => {
+                      return (
+                        <tr key={c?.id}>
+                          <td>{c.id}</td>
+                          <td>{c.localizedNames!["ja-JP"]}</td>
+                          <td>{c.localizedNames!["en-US"]}</td>
+                        </tr>
+                      );
+                    })}
+                  <tr></tr>
+                </tbody>
+              </table>
             </div>
             <div>
               <h2 className="font-bold">Map List</h2>
-              {data?.data.contents.maps.map((c: Map) => {
-                return (
-                  <div key={c.id}>
-                    {c.localizedNames && c.localizedNames["ja-JP"]}
-                  </div>
-                );
-              })}
+              <table className="table-auto">
+                <thead>
+                  <th>ID</th>
+                  <th>Localized Name - ja-JP</th>
+                  <th>Localized Name - en-US</th>
+                </thead>
+                <tbody>
+                  {contents.maps
+                    .filter((m) => !m?.name.includes("Null"))
+                    .map((m: Map) => {
+                      return (
+                        <tr key={m?.id}>
+                          <td>{m.id}</td>
+                          <td>{m.localizedNames!["ja-JP"]}</td>
+                          <td>{m.localizedNames!["en-US"]}</td>
+                        </tr>
+                      );
+                    })}
+                  <tr></tr>
+                </tbody>
+              </table>
             </div>
           </div>
-        )}
-        {error && <>{error.message}</>}
-      </main>
-    </React.StrictMode>
-  );
+        </main>
+      </React.StrictMode>
+    );
+  }
 }
